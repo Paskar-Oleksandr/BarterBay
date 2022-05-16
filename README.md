66f3f6f535eb691187592eafb7d75e8445bf3b1b
Paskar Oleksandr <paskar.olexandr@gmail.com>
2022-05-16 12:07:39 +0300

    BB-8-chore: basic infrastructures has been added Create maven-ci.yml

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BarterBay
+
+Status of last deployment: <br>
+<img src="https://github.com/Paskar-Oleksandr/BarterBay/workflows/Java-CI-With-Maven/badge.svg?branch=development"><br>
+
 Project for Academy
---
+++ src/main/java/com/barterbay/app/BarterBayApplication.java
@@ -0,0 +1,13 @@
+package com.barterbay.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarterBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarterBayApplication.class, args);
+	}
+
+}
