b547fa7edda5d93b0235ca24a8059596035284cc
Paskar Oleksandr <paskar.olexandr@gmail.com>
2022-11-17 19:48:27 +0200

    telegram bots have been deleted

--- src/main/java/com/barterbay/app/config/aws/S3Configuration.java
+++ src/main/java/com/barterbay/app/config/aws/S3Configuration.java
@@ -4,8 +4,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
-import com.amazonaws.services.s3.model.lifecycle.LifecycleFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,30 +23,14 @@ public class S3Configuration {
   @Value("${aws.s3.region.paris}")
   private String s3ParisRegionName;
 
-  @Value("${aws.s3.bucket-name.photos}")
-  private String bucketName;
-
   @Bean(name = "parisS3Client")
   public AmazonS3 getAmazonParisS3Client() {
     final var basicAWSCredentials = new BasicAWSCredentials(accessKeyId, accessKeySecret);
-    final var amazonS3 = AmazonS3ClientBuilder
+    return AmazonS3ClientBuilder
       .standard()
       .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
       .withRegion(s3ParisRegionName)
       .build();
-
-    amazonS3.setBucketLifecycleConfiguration(
-      bucketName,
-      new BucketLifecycleConfiguration()
-        .withRules(
-          new BucketLifecycleConfiguration.Rule()
-            .withId("bb-7days-expiration-rule-id")
-            .withFilter(new LifecycleFilter())
-            .withStatus(BucketLifecycleConfiguration.ENABLED)
-            .withExpirationInDays(7)
-        )
-    );
-    return amazonS3;
   }
 
   @Bean(name = "virginiaS3Client")
